8df9d1ac96cc042bc4819b3c6011291ca427d95e
Kamil Malesa <kamil.malesa1@gmail.com>
2022-07-19 18:55:09 +0200

    deleted unnecessary code

--- src/generateHolidayApplication.js
+++ src/generateHolidayApplication.js
@@ -3,62 +3,34 @@ import PizZip from "pizzip";
 import PizZipUtils from "pizzip/utils/index.js";
 import { saveAs } from "file-saver";
 
-function loadFile(url, callback) {
-  PizZipUtils.getBinaryContent(url, callback);
-}
+const generateHolidayApplication = (data) => {
+  const templatePath = "http://127.0.0.1:8080//application_template.docx";
 
-const generateHolidayApplication = (props) => {
-  loadFile(
-    "http://127.0.0.1:8080//wniosek_urlopowy.docx",
-    function (error, content) {
-      if (error) {
-        throw error;
-      }
-      var zip = new PizZip(content);
-      var doc = new Docxtemplater(zip, {
-        paragraphLoop: true,
-        linebreaks: true,
-      });
-      doc.setData(props);
-      try {
-        doc.render();
-      } catch (error) {
-        function replaceErrors(key, value) {
-          if (value instanceof Error) {
-            return Object.getOwnPropertyNames(value).reduce(function (
-              error,
-              key
-            ) {
-              error[key] = value[key];
-              return error;
-            },
-            {});
-          }
-          return value;
-        }
-        console.log(JSON.stringify({ error: error }, replaceErrors));
-
-        if (error.properties && error.properties.errors instanceof Array) {
-          const errorMessages = error.properties.errors
-            .map(function (error) {
-              return error.properties.explanation;
-            })
-            .join("\n");
-          console.log("errorMessages", errorMessages);
-        }
-        throw error;
-      }
-      var out = doc.getZip().generate({
-        type: "blob",
-        mimeType:
-          "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-      });
-      saveAs(
-        out,
-        `Urlop_${props.startDate}_${props.endDate}_${props.location}.docx`
-      );
+  PizZipUtils.getBinaryContent(templatePath, function (error, content) {
+    if (error) {
+      throw error;
     }
-  );
+    const zip = new PizZip(content);
+    const doc = new Docxtemplater(zip, {
+      paragraphLoop: true,
+      linebreaks: true,
+    });
+    doc.setData(data);
+    try {
+      doc.render();
+    } catch (error) {
+      console.log(error);
+    }
+    const out = doc.getZip().generate({
+      type: "blob",
+      mimeType:
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    });
+    saveAs(
+      out,
+      `Urlop_${data.startDate}_${data.endDate}_${data.location}.docx`
+    );
+  });
 };
 
 export default generateHolidayApplication;
