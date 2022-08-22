0a1e2f4ca91f3466338faba6e0584651fc07f107
Kamil Malesa <kamil.malesa1@gmail.com>
2022-08-22 21:41:13 +0200

    new component HolidayForm

---
+++ src/components/HolidayForm.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { DateRangePicker } from "rsuite";
+import "rsuite/dist/rsuite.css";
+
+const HolidayForm = () => {
+  return (
+    <>
+      <DateRangePicker />
+    </>
+  );
+};
+
+export default HolidayForm;
