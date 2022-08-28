8fce6e97580c99a369e464a1cbf86478848c3c34
Kamil <kamil.malesa1@gmail.com>
2022-08-28 13:24:49 +0200

    added post option graph call

--- src/graph.js
+++ src/graph.js
@@ -43,3 +43,67 @@ export function callMsGraphPatch(endPoint, accessToken) {
 
   return response;
 }
+
+export function callMsGraphPost(endPoint, accessToken, data) {
+  const headers = new Headers();
+  const bearer = `Bearer ${accessToken}`;
+
+  headers.append("Authorization", bearer);
+  headers.append(
+    "Content-Type",
+    "application/json;odata.metadata=minimal;odata.streaming=true;IEEE754Compatible=false"
+  );
+
+  const options = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify({
+      subject: `Urlop ${data.initials}`,
+      body: {
+        contentType: "text",
+        content: data.type,
+      },
+      start: {
+        dateTime: data.start,
+        timeZone: "Europe/Warsaw",
+      },
+      end: {
+        dateTime: data.end,
+        timeZone: "Europe/Warsaw",
+      },
+      location: {
+        displayName: data.location,
+      },
+      showAs: "oof",
+      // attendees: [
+      //   {
+      //     emailAddress: {
+      //       address: data.attendees[0].mail,
+      //       name: data.attendees[0].name,
+      //     },
+      //     type: "required",
+      //   },
+      //   {
+      //     emailAddress: {
+      //       address: data.attendees[1].mail,
+      //       name: data.attendees[1].name,
+      //     },
+      //     type: "required",
+      //   },
+      //   {
+      //     emailAddress: {
+      //       address: data.attendees[2].mail,
+      //       name: data.attendees[2].name,
+      //     },
+      //     type: "required",
+      //   },
+      // ],
+    }),
+  };
+
+  const response = fetch(endPoint, options)
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+
+  return response;
+}
